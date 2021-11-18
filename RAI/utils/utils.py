7960f0a0435aad3c91d768e5067f8ff6097c251c
cvattheu <christophervatt@gmail.com>
2021-11-18 11:07:25 -0800

    Redis filtering to remove Inf values

--- RAI/utils/utils.py
+++ RAI/utils/utils.py
@@ -8,6 +8,8 @@ def jsonify(v):
             return v.tolist()
         if type(v) in (np.bool,'_bool'):
             return bool(v)
-        
+        if type(v) is float and math.isinf(v):  # CURRENTLY REPLACING INF VALUES WITH NULL
+            return None
+
         return v
 
