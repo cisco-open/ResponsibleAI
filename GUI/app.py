96c81c53e7e8f99486492ced876e9bfebfab8d3d
cvattheu <christophervatt@gmail.com>
2021-11-19 17:10:59 -0800

    Dynamic time values

--- GUI/app.py
+++ GUI/app.py
@@ -6,6 +6,7 @@ from flask import Flask, url_for, redirect, render_template, request, abort, jso
 import flask_admin
 from flask_admin import helpers as admin_helpers
 import redis
+import datetime
 
 # Create Flask application
 app = Flask(__name__)
@@ -22,6 +23,17 @@ cert_meta = json.load(open(os.path.dirname(os.path.realpath(__file__)) + "\\outp
 r = redis.Redis(host='localhost', port=6379, db=0)
 
 cache = {'metric_info': json.loads(r.get('metric_info')), 'metric_values': r.lrange('metric_values', 0, -1)}
+
+
+def get_dates():
+    data_test = r.lrange('metric_values', 0, -1)
+    date_start = "2020-10-01"
+    if len(data_test) >= 1:
+        date_start = json.loads(data_test[0])['metadata > date'][:10]
+    now = datetime.datetime.now()
+    date_end = "{:02d}".format(now.year) + "-" + "{:02d}".format(now.month) + "-" + "{:02d}".format(now.day)
+    print("START: " + date_start + " END: " + date_end)
+    return date_start, date_end
 
 
 # Flask views
@@ -81,13 +93,15 @@ def event():
 
 @app.route('/getData/<date1>/<date2>', methods=['GET'])
 def getData(date1, date2):
-    date1 += "-00:00:00"
-    date2 += "-00:00:00"
+    date1 += " 00:00:00"
+    date2 += " 99:99:99"
     data_test = r.lrange('metric_values', 0, -1)
     # data_test = cache['metric_values']
     res = []
     for i in range(len(data_test)):
         item = json.loads(data_test[i])
+        print("DATE1: ", date1, ", DATE2:, ", date2, " DATE: ", item['metadata > date'])
+
         if date1 <= item['metadata > date'] <= date2:
             res.append(item)
     return json.dumps(res)
@@ -120,8 +134,8 @@ def getMetricInfo():
 
 @app.route('/getCertification/<date1>/<date2>', methods=['GET'])
 def getCertification(date1, date2):  # NOT REAL DATA YET.
-    date1 += "-00:00:00"
-    date2 += "-00:00:00"
+    date1 += " 00:00:00"
+    date2 += " 99:99:99"
     mask = (cert_measures['date'] >= date1) & (cert_measures['date'] <= date2)
     return cert_measures.loc[mask].to_json(orient='records')  # serialize and use JSON headers
 
@@ -134,29 +148,36 @@ def getCertificationMeta():
 @app.route('/viewClass/<category>')
 def renderClassTemplate(category):
     functional = category.replace(' ', '_').lower()
+    start_date, end_date = get_dates()
+
     return render_template('/admin/view_class.html',
                            admin_base_template=admin.base_template,
                            admin_view=admin.index_view,
                            get_url=url_for,
                            h=admin_helpers,
                            Category=category,
-                           Functional=functional)
+                           Functional=functional,
+                           start_date=start_date,
+                           end_date=end_date)
 
 
 @app.route('/viewAll')
 def renderAllMetrics():
+    start_date, end_date = get_dates()
     return render_template('/admin/view_all.html',
                            admin_base_template=admin.base_template,
                            admin_view=admin.index_view,
                            get_url=url_for,
-                           h=admin_helpers)
+                           h=admin_helpers,
+                           start_date=start_date,
+                           end_date=end_date)
 
 
 @app.route('/learnMore/<metric>')
 def learnMore(metric):
     data_test = r.get('metric_info')
     metric_info = json.loads(data_test)
-
+    start_date, end_date = get_dates()
     # metric_info = cache['metric_info']
     return render_template('/admin/metric_info.html',
                            admin_base_template=admin.base_template,
@@ -169,7 +190,9 @@ def learnMore(metric):
                            metric_explanation=metric_info[metric]['explanation'],
                            metric_type=metric_info[metric]['type'],
                            metric_tags=metric_info[metric]['tags'],
-                           metric_hidden_name=metric
+                           metric_hidden_name=metric,
+                           start_date=start_date,
+                           end_date=end_date
                            )
 
 
