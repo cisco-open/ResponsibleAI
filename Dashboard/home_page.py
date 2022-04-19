4ea2228b42306a0982936a17143796b6a151b2a3
Ali Payani <apayani@cisco.com>
2022-04-18 22:46:18 -0700

    added homepage mock

--- Dashboard/home_page.py
+++ Dashboard/home_page.py
@@ -2,5 +2,66 @@ import dash
 import dash_bootstrap_components as dbc
 from dash import Input, Output, dcc, html
 
+import dash_daq as daq
+
+def get_card( t1, t2, t3, ic, n , id, c):
+    return dbc.Card(
+    [
+        # html.I(className=ic, style={}),
+        # dbc.CardImg(src="fa-solid fa-list-check", top=True),
+        dbc.CardBody(
+            [
+                html.H4(t1, className="card-title"),
+                
+                daq.Gauge(
+    color={"gradient":True,"ranges":{"green":[0,6],"yellow":[6,8],"red":[8,10]}},
+    value=n,
+    label='',
+    max=10,
+    min=0,
+    size=120
+)
+
+
+                # daq.Knob(
+                # label=t2,
+                # value=n,
+                # color={"gradient":True,"ranges":{"green":[0,5],"yellow":[5,9],"red":[9,10]}}
+                # )
+                ,
+                # html.P(
+                #     t3,
+                #     className="card-text",
+                # ),
+                # dbc.Button("Go somewhere", color="primary"),
+            ]
+        ),
+    ],
+    style={"width": "20rem",
+    "background-color":c,
+    "margin":"20px",
+    "border-radius":"10px"},
+    )
+
+
 def get_home_page():
-    return html.P("This is the content of the home page!")
+
+
+
+    # return html.P("This is the content of the home page!")
+    return html.Div([
+        dbc.Row(
+             [
+                get_card("Explainability", "success rate", "details", "fa-solid fa-list-check", 7,"c1", "baige"),
+                get_card("Robustness", "success rate", "details", "fa-solid fa-list-check", 2,"c2", "lightgoldenrodyellow"),
+
+             ] )
+             ,
+        dbc.Row(
+             [
+                get_card("Performance", "success rate", "details", "fa-solid fa-list-check", 7,"c3","lightcyan"),
+                get_card("Fairness", "success rate", "details", "fa-solid fa-list-check", 2,"c4","peachpuff"),
+
+             ] )
+
+    ])
