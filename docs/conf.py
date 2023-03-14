c63615c12964688d4e979f67aa633b7d3c7e5a5f
shfarhee <shfarhee@cisco.com>
2023-03-14 12:47:30 -0500

    updated some files

--- docs/conf.py
+++ docs/conf.py
@@ -8,8 +8,7 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
-import RAI
+sys.path.insert(0, os.path.abspath('../RAI'))
 nbsphinx_allow_errors = True
 
 project = 'RAI Documentation'
