d70b224e433687088db5f1c5a9d6324303256cf2
shfarhee <shfarhee@cisco.com>
2023-03-14 16:01:33 -0500

    updated some files

--- docs/conf.py
+++ docs/conf.py
@@ -8,9 +8,11 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../'))
+#sys.path.insert(0, os.path.abspath('../'))
 nbsphinx_allow_errors = True
 
+#so we can import RAI
+sys.path.append(os.path.join(os.path.dirname(__name__), ".."))
 
 
 project = 'RAI Documentation'
