782028fbe6ca6089f4cf2bfbe26fdea6bd08514c
shfarhee <shfarhee@cisco.com>
2023-03-15 18:05:18 -0500

    updated some files

--- docs/conf.py
+++ docs/conf.py
@@ -65,7 +65,7 @@ intersphinx_mapping = {
     'python': ('https://docs.python.org/3/', None),
     'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
 }
-intersphinx_disabled_domains = ['std']
+#intersphinx_disabled_domains = ['std']
 
 
 napoleon_include_init_with_doc = True
