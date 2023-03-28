efa3f0c6a676d71719523563b79bb3d915cf43d0
Ali Payani <apayani@cisco.com>
2023-03-28 11:22:42 -0700

    reformat the demo file

--- demos/adult_demo_grid_search.py
+++ demos/adult_demo_grid_search.py
@@ -14,56 +14,67 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-
+#importing modules
 import os
 import sys
 import inspect
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
 
+# importing RAI modules
 parent = os.path.abspath('.')
 sys.path.insert(1, parent)
-
 from RAI.AISystem import AISystem, Model
 from RAI.dataset import NumpyData, Dataset
 from RAI.redis import RaiRedis
 from RAI.utils import df_to_RAI
-from sklearn.ensemble import RandomForestClassifier
 
-
-currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-parentdir = os.path.dirname(currentdir)
-sys.path.insert(0, parentdir)
-
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+# Configuration
 use_dashboard = True
 data_path = "./data/adult/"
 
+
+#setup path
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir)
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+#loading train and test data
 train_data = pd.read_csv(data_path + "train.csv", header=0, skipinitialspace=True, na_values="?")
 test_data = pd.read_csv(data_path + "test.csv", header=0, skipinitialspace=True, na_values="?")
 all_data = pd.concat([train_data, test_data], ignore_index=True)
 
-# convert aggregated data into RAI format
+# Preprocess data and split into train and test sets
 meta, X, y, output = df_to_RAI(all_data, target_column="income-per-year", normalize="Scalar", max_categorical_threshold=5)
 xTrain, xTest, yTrain, yTest = train_test_split(X, y, random_state=1, stratify=y)
 
+# create and train the base model
 clf = RandomForestClassifier(n_estimators=10, criterion='entropy', random_state=0, min_samples_leaf=5, max_depth=2)
 clf.fit(xTrain, yTrain)
+
+# initialize the model with the base classifer
 model = Model(agent=clf, output_features=output, name="cisco_income_ai", predict_fun=clf.predict, predict_prob_fun=clf.predict_proba,
             description="Income Prediction AI", model_class="Random Forest Classifier")
 configuration = {"fairness": {"priv_group": {"race": {"privileged": 1, "unprivileged": 0}},
                             "protected_attributes": ["race"], "positive_label": 1},
                 "time_complexity": "polynomial"}
 
+# setup the dataset
 dataset = Dataset({"train": NumpyData(xTrain, yTrain, xTrain), "test": NumpyData(xTest, yTest, xTest)})
+
+# initialize RAI 
 ai = AISystem(name="AdultDB_GridSearch", task='binary_classification', meta_database=meta, dataset=dataset, model=model)
 ai.initialize(user_config=configuration)
+
 
 if use_dashboard:
     r = RaiRedis(ai)
     r.connect()
     r.reset_redis()
     
+#  function to evaluate each model     
 def test_model(mdl, name):
     mdl.fit(xTrain, yTrain)
     ai.model.agent = mdl
@@ -89,9 +100,7 @@ def main():
     if use_dashboard:
         r.export_visualizations("test", "test")
 
-
-    
-    
+ 
 
 if __name__ == "__main__":
     
